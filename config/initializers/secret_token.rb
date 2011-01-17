b46d761a0a5b8d700cf509de00e1b260e0edbbc8
Slobodan Kovacevic <basti@orangeiceberg.com>
2011-01-17 10:54:48 +0100

    Created new Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharedearthapp::Application.config.secret_token = '7d956925c518445324540e63c3ed1146a57f7cc380ef426c10778692e6dcec46fcdf11df12506603afc9062906e8ed66f15b777cad2f2afe7bc69092fa399ff3'
