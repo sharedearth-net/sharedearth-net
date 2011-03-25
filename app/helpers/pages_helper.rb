2256458675cfad71d24664f33adfdbda3824c9c6
Nebojsa Zoric <zoricn@gmail.com>
2011-03-25 13:08:38 +0100

    Corrected sentances

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -12,17 +12,17 @@ module PagesHelper
     sentance = ""
     case event_type
     when EventType.find(1).name
-      sentance = requester + " requested your " + item
+      sentance = " You started sharing your " + item
     when EventType.find(2).name
       sentance = requester + " requested your " + item
     when EventType.find(3).name
-      sentance =  "You requested " + requester_possesive +  " " +  item
+      sentance =  "You requested " + gifter_possesive +  " " +  item
     when EventType.find(4).name
       sentance = "You accepted " + requester_possesive + " request to share your " +  item
     when EventType.find(5).name
       sentance = "You rejected " + requester_possesive + " request to share your " +  item
     when EventType.find(6).name
-      sentance = requester + " accepted your request to share their " + item + " with you"
+      sentance = gifter + " accepted your request to share their " + item + " with you"
     when EventType.find(7).name
       sentance = gifter + " rejected your request to share their " + item
     when EventType.find(8).name
@@ -34,17 +34,17 @@ module PagesHelper
     when EventType.find(11).name
       sentance =  "You completed the action sharing " + gifter_possesive + " " + item
     when EventType.find(12).name
-      sentance =  "You completed the action sharing your " + link_to_item + " with " + gifter_possesive
+      sentance =  "You completed the action sharing your " + item + " with " + requester
     when EventType.find(13).name
-      sentance =  gifter_possesive + " completed the action sharing their " + item + " with you "
+      sentance =  gifter + " completed the action sharing their " + item + " with you "
     when EventType.find(14).name
       sentance =  "You canceled the action sharing your " +  item + " with " + requester
     when EventType.find(15).name
-      sentance =  gifter_possesive + " canceled the action sharing their " + item + " with you "
+      sentance =  gifter + " canceled the action sharing their " + item + " with you "
     when EventType.find(16).name
       sentance =  requester + " canceled the request for your " + item
     when EventType.find(17).name
-      sentance =  "You canceled the request for your " + item
+      sentance =  "You canceled the request for your " + gifter_possesive + " " + item
     else
       #
     end
