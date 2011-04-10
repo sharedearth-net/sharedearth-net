43eb41da0b690cdd30d59eee9c115c858f9325a4
Nele <visin2@gmail.com>
2011-04-10 22:11:24 +0200

    Event entity new scope

--- app/models/event_entity.rb
+++ app/models/event_entity.rb
@@ -1,4 +1,6 @@
 class EventEntity < ActiveRecord::Base
   belongs_to :event_log
   belongs_to :entity, :polymorphic => true # person, community, ... anyone and anything related to the event
+  
+  scope :involves, lambda { |entity| where("(entity_id = ? AND entity_type = ?)", entity.id, entity.class.to_s) }
 end
