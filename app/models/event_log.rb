57e7a324e83676b33456d38ebfbf5a632c1e2ec1
Nele <visin2@gmail.com>
2011-04-10 22:11:00 +0200

    New more general method for storing event logs into database, new scope and few methods

--- app/models/event_log.rb
+++ app/models/event_log.rb
@@ -21,6 +21,8 @@ class EventLog < ActiveRecord::Base
   belongs_to :event_type
   
   has_many :event_entities
+  
+  scope :involves, lambda { |entity| where("(primary_id = ? AND primary_type = ?) OR (secondary_id = ? AND secondary_type = ?) ", entity.id, entity.class.to_s, entity.id, entity.class.to_s) }
 
   validates_presence_of :primary_id, :primary_type, :primary_short_name, :primary_full_name
   # validates_presence_of :action_object_id, :action_object_type, :action_object_type_readable
@@ -45,6 +47,66 @@ class EventLog < ActiveRecord::Base
     
     EventEntity.create!(:event_log => event_log, :entity => first_person)
     EventEntity.create!(:event_log => event_log, :entity => second_person)
+    
+    event_log
+  end
+  
+  def involved_as_requester?(person)
+    self.primary_id == person.id
+  end
+  
+  def involved_as_gifter?(person)
+    self.secondary_id == person.id
+  end
+  
+    
+  # One method to log all events
+  # First param: first involved subject
+  # Second param: second involved subject - optional
+  # Third param: Item or Skill this ActivityLog is connected to - optional
+  # Forth param: Type of the event that occured
+  # #####
+  # USE: If there is no second person nor object in event, just use nil on those places
+  
+  def self.create_news_event_log(first_person, second_person, object, event_type)
+    if(second_person.nil?)
+      second_person_first_name = nil
+      second_person_name = nil
+    else
+      second_person_first_name = second_person.first_name
+      second_person_name = second_person.name    
+    end
+    
+    if(object.nil?)
+      action_object_type_readable = nil
+    else
+      action_object_type_readable = object.item_type
+    end
+    
+    event_log = EventLog.create!(
+      :primary => first_person,
+      :primary_short_name => first_person.first_name,
+      :primary_full_name => first_person.name,
+      :action_object => object,
+      :action_object_type_readable => action_object_type_readable,
+      :secondary => second_person,
+      :secondary_short_name => second_person_first_name,
+      :secondary_full_name => second_person_name,
+      :related => nil,
+      :event_type_id => event_type
+    )
+    
+    if(!first_person.nil?) 
+      EventEntity.create!(:event_log => event_log, :entity => first_person)
+    end
+    
+    if(!second_person.nil?)
+      EventEntity.create!(:event_log => event_log, :entity => second_person)
+    end
+    
+    if(!object.nil?)
+      EventEntity.create!(:event_log => event_log, :entity => object)
+    end
     
     event_log
   end
