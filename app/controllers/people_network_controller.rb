1d4ead96791d91655fd90b2df0f2da9379a138d0
Nele <visin2@gmail.com>
2011-04-10 22:04:14 +0200

    Save to event log if relationship broken

--- app/controllers/people_network_controller.rb
+++ app/controllers/people_network_controller.rb
@@ -5,6 +5,7 @@ class PeopleNetworkController < ApplicationController
   def destroy
     @people_network = PeopleNetwork.find(params[:id])
     trusted_person = @people_network.trusted_person
+    EventLog.create_news_event_log(@people_network.person, @people_network.trusted_person, nil, EventType.trust_withdrawn)
     PeopleNetwork.involves(@people_network.person).involves(@people_network.trusted_person).limit(2).destroy_all
     redirect_to(trusted_person, :notice => I18n.t('messages.people_network.destroy'))
   end
