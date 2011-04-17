ce4db4addde9be607981b8f2bd412dfd982cf49f
Nebojsa <zoricn@gmail.com>
2011-04-17 21:26:09 +0200

    diplay model

---
+++ app/models/event_display.rb
@@ -0,0 +1,6 @@
+class EventDisplay < ActiveRecord::Base
+  # t.integer :type_id - id for which page or entity event log is generated
+  # t.integer  :person_id - id of person to whom the event log was displayed
+  # t.integer :event_id - id of the event that was shown to the person
+  DASHBOARD_FEED = 10.freeze
+end
