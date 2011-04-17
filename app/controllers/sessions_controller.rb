987573952089c11d307c9567be02cdc030489029
Nebojsa <zoricn@gmail.com>
2011-04-17 21:25:37 +0200

    Save new user with token - new field added to users table

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -37,7 +37,10 @@ class SessionsController < ApplicationController
   ##
   def create
     auth = request.env["omniauth.auth"]
+    token = auth["credentials"]["token"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user.token = token
+    user.save!
     session[:user_id] = user.id
     redirect_to root_url, :notice => I18n.t('messages.signed_in')
   end
