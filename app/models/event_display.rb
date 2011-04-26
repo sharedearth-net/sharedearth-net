7564ab9efc4ab44b109301348c9e754baacabd73
Nebojsa <zoricn@gmail.com>
2011-04-26 17:39:12 +0200

    Added recent activity feed constant

--- app/models/event_display.rb
+++ app/models/event_display.rb
@@ -3,4 +3,5 @@ class EventDisplay < ActiveRecord::Base
   # t.integer  :person_id - id of person to whom the event log was displayed
   # t.integer :event_id - id of the event that was shown to the person
   DASHBOARD_FEED = 10.freeze
+  RECENT_ACTIVITY_FEED = 20.freeze
 end
