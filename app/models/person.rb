bc86b9295de5cab270d4f9f2d9db934716799385
Dan Gooden <dani.mcgoo@gmail.com>
2011-04-30 19:56:28 +1000

    Modified news feed SQL so it shows all trusted network events and any events relating to the user logged in

--- app/models/person.rb
+++ app/models/person.rb
@@ -84,17 +84,8 @@ class Person < ActiveRecord::Base
   end
 
   def news_feed
-    # trying to replicate following query using AREL
-    # SELECT
-    # event_log_id, SUM(trusted_relationship_value) as total_relationship_value
-    # FROM
-    # event_entities ee
-    # INNER JOIN
-    #      (SELECT trusted_person_id, 4 as trusted_relationship_value FROM people_networks WHERE person_id = XYZ) network
-    #      ON ee.entity_id = network.trusted_person_id AND ee.entity_type = 'Person'
-    # GROUP BY ee.event_log_id
-    # ORDER BY ee.created_at DESC
-    # LIMIT 25
+    # Updated SQL to get all events relating to anyone in a user's trusted
+    # network or to themselves
    
 		ee = Arel::Table.new(EventEntity.table_name.to_sym)
     pn = Arel::Table.new(PeopleNetwork.table_name.to_sym)
@@ -102,8 +93,10 @@ class Person < ActiveRecord::Base
     pn_network = pn.project(pn[:trusted_person_id], Arel.sql("4 as trusted_relationship_value")).where(pn[:person_id].eq(self.id))
 
     query = ee.project(Arel.sql("#{ee.name}.event_log_id as event_log_id"), Arel.sql("SUM(trusted_relationship_value) as total_relationship_value"))
-    query = query.join(Arel.sql("INNER JOIN (#{pn_network.to_sql}) AS network ON #{ee.name}.entity_id = network.trusted_person_id AND #{ee.name}.entity_type = 'Person'"))
+    query = query.join(Arel.sql("LEFT JOIN (#{pn_network.to_sql}) AS network ON #{ee.name}.entity_id = network.trusted_person_id AND #{ee.name}.entity_type = 'Person'"))
     query = query.group(ee[:event_log_id], ee[:created_at]).order("#{ee.name}.created_at DESC").take(25)
+    query = query.where(Arel.sql("trusted_person_id IS NOT NULL or (#{ee.name}.entity_type = 'Person' and #{ee.name}.entity_id = #{self.id})"))
+
     event_log_ids = EventEntity.find_by_sql(query.to_sql)
 
 		# CASHE PREVIOUSLY SHOWN NEWS FEED IF NOT ALREADY CASHED
