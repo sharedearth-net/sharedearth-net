a1c3d3fc6b7f5710e42702720cca13e80cd1dd23
Nebojsa <zoricn@gmail.com>
2011-05-05 15:24:03 +0200

    When item request is completed redirect to create new feedback

--- app/controllers/item_requests_controller.rb
+++ app/controllers/item_requests_controller.rb
@@ -76,7 +76,7 @@ class ItemRequestsController < ApplicationController
   
   def complete
     @item_request.complete!(current_user)
-    redirect_to(dashboard_path, :notice => I18n.t('messages.item_requests.request_completed'))
+    redirect_to(new_request_feedback_path(@item_request), :notice => I18n.t('messages.item_requests.request_completed'))
   end
   
   private
