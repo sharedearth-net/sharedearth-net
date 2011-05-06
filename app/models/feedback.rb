aafbfcb56e84ddcf2655c6d6be40db4c19888189
Nebojsa <zoricn@gmail.com>
2011-05-06 16:36:49 +0200

    Added method that get called when feedback is done so reputation level can be changed

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,6 +2,12 @@ class Feedback < ActiveRecord::Base
   attr_accessible :item_request_id, :person_id, :feedback, :feedback_note
   belongs_to :item_request
   
+  after_create :feedback_reputation_count
+  
   scope :as_person, lambda { |entity| where("person_id = ?", entity.id) }
+  
+  def feedback_reputation_count
+     
+  end
   
 end
