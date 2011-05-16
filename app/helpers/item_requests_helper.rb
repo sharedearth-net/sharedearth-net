af33e4c618e0ca630b11dc26f1be06ae9d25d846
Nebojsa <zoricn@gmail.com>
2011-05-16 11:53:54 +0200

    Feedback box colour based on feedback

--- app/helpers/item_requests_helper.rb
+++ app/helpers/item_requests_helper.rb
@@ -1,2 +1,15 @@
 module ItemRequestsHelper
+  def feedback_box_colour(feedback)
+    case feedback
+      when Feedback::FEEDBACK_POSITIVE
+        html = "<div class=\"feedback positive-feedback\"></div>"
+      when Feedback::FEEDBACK_NEGATIVE
+        html = "<div class=\"feedback negative-feedback\"></div>"
+      when Feedback::FEEDBACK_NEUTRAL
+        html = "<div class=\"feedback neutral-feedback\"></div>"
+      else
+        #
+    end
+    html.html_safe
+  end
 end
