ec5f34811b8c94450f8ffe408a0ef42bfe841da3
Nebojsa <zoricn@gmail.com>
2011-05-17 23:15:32 +0200

    Fixed migration for clean pulls

--- db/migrate/20110429105506_add_reputation_to_person.rb
+++ db/migrate/20110429105506_add_reputation_to_person.rb
@@ -2,7 +2,7 @@ class AddReputationToPerson < ActiveRecord::Migration
   def self.up
     people = Person.all
     people.each do | person |
-      ReputationRating.create(:person_id => person.id, :gift_actions => 0,:distinct_people => 0, 
+      ReputationRating.create!(:person_id => person.id, :gift_actions => 0,:distinct_people => 0, 
                               :total_actions => 0, :positive_feedback => 0, :negative_feedback => 0, :neutral_feedback => 0,
                               :requests_received => 0,  :requests_answered => 0, :trusted_network_count => 0,  :activity_level => 0)
     end
