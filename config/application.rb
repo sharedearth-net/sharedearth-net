8b2a8676db6d9de49a07d5d4eb58345a3306bc38
Nebojsa <zoricn@gmail.com>
2011-05-17 23:14:58 +0200

    Use jQuery as default javascript, can be changed later to something else

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module Sharedearthapp
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    #Change :defaults to load jQuery insted of Prototype
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
   end
 end
