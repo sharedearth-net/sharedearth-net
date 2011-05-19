4b9783bfdd82665f2792c3e59bed06d90d815239
Nebojsa <zoricn@gmail.com>
2011-05-19 12:15:40 +0200

    removed specs for those methods

--- spec/controllers/feedbacks_controller_spec.rb
+++ spec/controllers/feedbacks_controller_spec.rb
@@ -1,7 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe FeedbacksController do
-  fixtures :all
   render_views
 
   it "new action should render new template" do
@@ -18,23 +17,6 @@ describe FeedbacksController do
   it "create action should redirect when model is valid" do
     Feedback.any_instance.stubs(:valid?).returns(true)
     post :create
-    response.should redirect_to(root_url)
-  end
-
-  it "edit action should render edit template" do
-    get :edit, :id => Feedback.first
-    response.should render_template(:edit)
-  end
-
-  it "update action should render edit template when model is invalid" do
-    Feedback.any_instance.stubs(:valid?).returns(false)
-    put :update, :id => Feedback.first
-    response.should render_template(:edit)
-  end
-
-  it "update action should redirect when model is valid" do
-    Feedback.any_instance.stubs(:valid?).returns(true)
-    put :update, :id => Feedback.first
     response.should redirect_to(root_url)
   end
 end
