b25fba2c3b57b6061b27c50a6981243ff9efba92
Nebojsa <zoricn@gmail.com>
2011-05-19 11:57:33 +0200

    Spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,43 +1,59 @@
-require 'simplecov'
-SimpleCov.start
+require 'rubygems'
+require 'spork'
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require "paperclip/matchers"
-require 'factory_girl'
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require "paperclip/matchers"
+  require 'factory_girl'
+  require 'ffaker'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  begin
+    Dir[('factories/*.rb')].each {|f| require f } 
+  rescue Factory::DuplicateDefinitionError
+    nil
+  end
 
-#Load all factories and report if there is duplicate definition
-begin
-  Dir[('factories/*.rb')].each {|f| require f } 
-rescue Factory::DuplicateDefinitionError
-  nil
+  RSpec.configure do |config|
+    # == Mock Framework
+    #
+    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    # config.mock_with :mocha
+    # config.mock_with :flexmock
+    # config.mock_with :rr
+    config.mock_with :rspec
+
+    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, remove the following line or assign false
+    # instead of true.
+    config.use_transactional_fixtures = true
+
+    config.include Paperclip::Shoulda::Matchers
+    config.include(ControllerMacros, :type => :controller)
+    config.include(UserMocks)
+  end
+
 end
 
-RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  config.mock_with :rspec
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
-  config.include Paperclip::Shoulda::Matchers
-  config.include(ControllerMacros, :type => :controller)
-  config.include(UserMocks)
+Spork.each_run do
+  # This code will be run each time you run your specs.
+  #Load all factories and report if there is duplicate definition
 end
+
+#Enable to have coverage tool
+#require 'simplecov'
+#SimpleCov.start
+
+
+
