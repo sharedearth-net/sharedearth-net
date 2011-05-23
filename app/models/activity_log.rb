ee9e9f5a8b6cf381b3a0b2ca994247b51e3b4d46
Nebojsa <zoricn@gmail.com>
2011-05-23 11:36:25 +0200

    More general method for saving activity logs

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -24,11 +24,12 @@ class ActivityLog < ActiveRecord::Base
   # [primary] accepted [secondary]'s [related] your [action_object]
   # Note: this just illustrates what primary, secondary, related and action_object are.
   #       Technically speaking sentence is not generated like that.
-
+  
+  #Action object and related object is not obligatory when two persons are establishing trust
   validates_presence_of :primary_id, :primary_type
   validates_presence_of :event_code
-  validates_presence_of :action_object_id, :action_object_type, :action_object_type_readable
-  validates_presence_of :related_id, :related_type
+  #validates_presence_of :action_object_id, :action_object_type, :action_object_type_readable
+  #validates_presence_of :related_id, :related_type
   
   scope :gift_actions, lambda { |entity| where("(primary_id = ? and event_type_id IN (?) ) ", entity.id, EventType.completed_request_ids) }
   scope :involves, lambda { |entity| where("(primary_id = ? AND primary_type = ?) OR (secondary_id = ? AND secondary_type = ?) ", entity.id, entity.class.to_s, entity.id, entity.class.to_s) }
@@ -125,6 +126,63 @@ class ActivityLog < ActiveRecord::Base
        :event_type_id => EventType.add_item
      )
    end
+   
+  # More general method for saving activity logs
+  # First param: item_request object from which activity log will be created
+  # Second param: Type of the event for gifter
+  # Third param: Type of the event for the requester 
+  # #####
+  
+  def self.create_activity_log(first_person, second_person, object, event_type_requester, event_type_gifter)
+    event_code = ActivityLog.next_event_code
+    if(second_person.nil?)
+      second_person_first_name = nil
+      second_person_name = nil
+    else
+      second_person_first_name = second_person.first_name
+      second_person_name = second_person.name    
+    end
+    
+    if(first_person.nil?)
+      first_person_first_name = nil
+      first_person_name = nil
+    else
+      first_person_first_name = first_person.first_name
+      first_person_name = first_person.name    
+    end
+    
+    if(object.nil?)
+      action_object_type_readable = nil
+    else
+      action_object_type_readable = object.item_type
+    end
+
+    # create log for gifter
+    ActivityLog.create!(
+      :event_code => event_code,
+      :primary => first_person,
+      :action_object => object,
+      :action_object_type_readable => action_object_type_readable,
+      :secondary => second_person,
+      :secondary_short_name => second_person.first_name,
+      :secondary_full_name => second_person.name,
+      :related => nil,
+      :event_type_id => event_type_gifter
+    )
+
+    # create log for requester
+    ActivityLog.create!(
+      :event_code => event_code,
+      :primary => second_person,
+      :action_object => object,
+      :action_object_type_readable => action_object_type_readable,
+      :secondary => first_person,
+      :secondary_short_name => first_person.first_name,
+      :secondary_full_name => first_person.name,
+      :related => nil,
+      :event_type_id => event_type_requester
+    )
+  end
 
 
 end
