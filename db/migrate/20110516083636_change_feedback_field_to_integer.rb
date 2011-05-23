90260e647e5b4b1bbdab53d91477e5af1bc1a8ad
Nebojsa <zoricn@gmail.com>
2011-05-23 10:28:01 +0200

    Overriding PostgreSql differences over LiteSQL database on Heroku dev. mashine

--- db/migrate/20110516083636_change_feedback_field_to_integer.rb
+++ db/migrate/20110516083636_change_feedback_field_to_integer.rb
@@ -1,24 +1,25 @@
 class ChangeFeedbackFieldToInteger < ActiveRecord::Migration
   def self.up
+    add_column :feedbacks, :feedback_int, :integer
     feedbacks = Feedback.all
     feedbacks.each do |f|
       case f.feedback
         when 'positive'
-          f.feedback = Feedback::FEEDBACK_POSITIVE
+          f.feedback_int = Feedback::FEEDBACK_POSITIVE
         when 'negative'
-          f.feedback = Feedback::FEEDBACK_NEGATIVE
+          f.feedback_int = Feedback::FEEDBACK_NEGATIVE
         when 'neutral'
-          f.feedback = Feedback::FEEDBACK_NEUTRAL
+          f.feedback_int = Feedback::FEEDBACK_NEUTRAL
         else
           #    
       end
       f.save!
     end
-    change_table :feedbacks do |t|
-      t.change :feedback, :integer
-    end
+    remove_column :feedbacks, :feedback
+    rename_column :feedbacks, :feedback_int, :feedback
   end
 
   def self.down
+    remove_column :feedbacks, :feedback_int
   end
 end
