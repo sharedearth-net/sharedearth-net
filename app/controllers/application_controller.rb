3da74508f23fb71b5ad8f505a8cdfb137dbcacfe
Nebojsa <zoricn@gmail.com>
2011-05-24 11:19:22 +0200

    Redirect back method with optional parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :current_user
+  helper_method :current_user, :redirect_to_back
   
   layout :dynamic_layout
 
@@ -20,5 +20,18 @@ class ApplicationController < ActionController::Base
   # Control which layout is used.
   def dynamic_layout
     current_user.nil? ? 'welcome' : 'application'
+  end
+  
+  #Redirection with optional notice
+  def redirect_to_back(options = {}, default = dashboard_path)
+    if has_referer?
+      redirect_to :back, :notice => options[:notice]
+    else
+      redirect_to default, :notice => options[:notice]
+    end
+  end
+  
+  def has_referer?
+    !request.env["HTTP_REFERER"].blank? and request.env["HTTP_REFERER"] != request.env["REQUEST_URI"]
   end
 end
