64d955381eff65c73b212b9595b98efac32f793d
Nebojsa <zoricn@gmail.com>
2011-06-04 16:37:30 +0200

    Fixed SSL issue when updated omniauth gem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '101448083266993', 'e6fd636f8f479f62f9e62ce76d9bd8f9'
+  provider :facebook, '101448083266993', 'e6fd636f8f479f62f9e62ce76d9bd8f9', {:scope => 'publish_stream,email', :client_options => {:ssl => {:ca_path => '/etc/ssl/certs'}}}
+end
+if Rails.env.development?
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE 
 end
