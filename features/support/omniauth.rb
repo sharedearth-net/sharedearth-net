e1199a843afbb75bfbe5adcddde8916fcaa72eb3
Nebojsa <zoricn@gmail.com>
2011-06-04 12:00:29 +0200

    Omniauth mock model

--- features/support/omniauth.rb
+++ features/support/omniauth.rb
@@ -14,12 +14,26 @@ Before("@omniauth_test") do
   OmniAuth.config.mock_auth[:facebook] = {
       "uid" => "12345",
       "provider" => "facebook",
-      "user_info" => { "name" => "Johny" },
-      "credentials" => { "token" => "exampletoken" },
+      "user_info" => { "nickname" => "Johny", "name" => "John"},
+      "credentials" => { "token" => "101448083266993|e988f10d01ea27bff083648b.1-625817457|vbqk5KMmrxogpCzR9A2JFA_KEBg" },
       "extra" => { "user_hash" => FACEBOOK_INFO }
   }
 end
 
 After("@omniauth_test") do
   OmniAuth.config.test_mode = false
+end
+
+
+def omniauth_mock_facebook
+  OmniAuth.config.test_mode = true
+  # the symbol passed to mock_auth is the same as the name of the provider set up in the initializer
+  OmniAuth.config.mock_auth[:facebook] = {
+      "uid" => "12345",
+      "provider" => "facebook",
+      "user_info" => { "nickname" => "Johny", "name" => "John"},
+      "credentials" => { "token" => "101448083266993|e988f10d01ea27bff083648b.1-625817457|vbqk5KMmrxogpCzR9A2JFA_KEBg" },
+      "extra" => { "user_hash" => FACEBOOK_INFO }
+  }
+
 end
