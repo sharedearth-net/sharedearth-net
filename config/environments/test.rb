07fac0245a6b4cb4abe90a599d1b4fd5adc42ab3
Nebojsa <zoricn@gmail.com>
2011-06-07 12:49:24 +0200

    CAPYBARA GEM - FIX FOR INCOPATIBILITY BETWEEN RACK AND CAPYBARA for cucumber testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,3 +33,10 @@ Sharedearthapp::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 end
+
+#Fix for Capybara losing session with omniauth. Rake uses example.org but Capybara uses www.example.com and it breaks session
+module Rack
+  module Test
+    DEFAULT_HOST = "www.example.com"
+  end
+end
