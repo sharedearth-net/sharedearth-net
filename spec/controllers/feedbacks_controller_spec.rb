43ccfa35a2b1599e8101383e9be6911842780f33
Nebojsa <zoricn@gmail.com>
2011-06-10 14:31:34 +0200

    Feedback controler spec

--- spec/controllers/feedbacks_controller_spec.rb
+++ spec/controllers/feedbacks_controller_spec.rb
@@ -1,22 +1,25 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe FeedbacksController do
   render_views
 
   it "new action should render new template" do
+    feedback = Factory(:feedback)
     get :new
-    response.should render_template(:new)
+    response.should render_template(new_request_feedback_path(feedback.item_request))
   end
 
   it "create action should render new template when model is invalid" do
-    Feedback.any_instance.stubs(:valid?).returns(false)
+    feedback = Factory(:feedback)
+    feedback.stub!(:valid?).returns(false)
     post :create
-    response.should render_template(:new)
+    
+    response.should render_template(new_request_feedback_path(feedback.item_request))
   end
 
   it "create action should redirect when model is valid" do
-    Feedback.any_instance.stubs(:valid?).returns(true)
+    Feedback.any_instance.stub!(:valid?).returns(true)
     post :create
-    response.should redirect_to(root_url)
+    response.should redirect_to(dashboard_path)
   end
 end
