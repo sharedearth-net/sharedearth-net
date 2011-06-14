a67c34f7be6b304a2dcbee02e2a8602c518c95d4
Nebojsa <zoricn@gmail.com>
2011-06-14 12:47:55 +0200

    Optimised specs with factory girl

--- spec/views/items/index.html.erb_spec.rb
+++ spec/views/items/index.html.erb_spec.rb
@@ -2,30 +2,13 @@ require 'spec_helper'
 
 describe "items/index.html.erb" do
   before(:each) do
-    assign(:items, [
-      stub_model(Item,
-        :item_type => "MyItemType",
-        :name => "MyItemName",
-        :description => "MyText",
-        :owner_id => 1,
-        :owner_type => "Owner Type"
-      ),
-      stub_model(Item,
-        :item_type => "MyItemType",
-        :name => "MyItemName",
-        :description => "MyText",
-        :owner_id => 1,
-        :owner_type => "Owner Type"
-      )
-    ])
+    assign(:items, [Factory(:item), Factory(:item)])
   end
 
   it "renders a list of items" do
     render
-    assert_select "tr>td", :text => "MyItemType".to_s, :count => 2
-    assert_select "tr>td", :text => "MyItemName".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    # assert_select "tr>td", :text => 1.to_s, :count => 2
-    # assert_select "tr>td", :text => "Owner Type".to_s, :count => 2
+    rendered.should contain("Mountainbike"), :count => 2
+    rendered.should contain("Bike"), :count => 2
+    rendered.should contain("Big beautifyl bike"), :count => 2
   end
 end
