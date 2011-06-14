b2971e8ea4861e2561889651f9f036608185604f
Nebojsa <zoricn@gmail.com>
2011-06-14 12:48:12 +0200

    items show spec

--- spec/views/items/show.html.erb_spec.rb
+++ spec/views/items/show.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "items/show.html.erb" do
 
-  let(:signedin_user) { generate_mock_user_with_person }
+  let(:signedin_user) { Factory(:person) }
   
   def as_item_owner
     view.stub(:current_user).and_return(stub_model(User))
@@ -16,24 +16,18 @@ describe "items/show.html.erb" do
   
   before(:each) do
     stub_template "shared/_trust_profile.html.erb" => "Trust profile"
-    view.stub(:current_user).and_return(signedin_user)
+    view.stub(:current_user).and_return(signedin_user.user)
 
-    @item = assign(:item, stub_model(Item,
-      :item_type => "MyItemType",
-      :name => "MyItemName",
-      :description => "MyItemDescription",
-      :owner_id => 1,
-      :owner_type => "Person",
-      :status => Item::STATUS_NORMAL
-    ))
+    @item = Factory(:item, :owner => signedin_user)
+    @item.stub(:available?).and_return(true)
   end
 
   it "renders attributes in <p>" do
     view.stub(:owner_only)
     render
-    rendered.should match(/MyItemType/)
-    rendered.should match(/MyItemName/)
-    rendered.should match(/MyItemDescription/)
+    rendered.should match(/Bike/)
+    rendered.should match(/Mountainbike/)
+    rendered.should match(/Big beautifyl bike/)
   end
   
   it "should render edit link to item owner" do
@@ -78,6 +72,13 @@ describe "items/show.html.erb" do
     rendered.should have_selector("a", :href => item_path(@item))
   end
   
+  it "should not render 'delete' link if item is currently shared" do
+    as_item_owner
+    @item.stub(:available?).and_return(false)
+    render
+    rendered.should_not have_selector("a", :href => item_path(@item))
+  end
+  
   describe "for lost item" do
     before do
       @item.status = Item::STATUS_LOST
@@ -87,6 +88,13 @@ describe "items/show.html.erb" do
       as_item_owner
       render
       rendered.should have_selector("a", :href => mark_as_normal_item_path(@item))
+    end
+    
+    it "should not render 'mark as found' link if item is not lost" do
+      as_item_owner
+      @item.stub(:lost?).and_return(false)
+      render
+      rendered.should_not have_selector("a", :href => mark_as_normal_item_path(@item))
     end
   end
 
@@ -99,6 +107,13 @@ describe "items/show.html.erb" do
       as_item_owner
       render
       rendered.should have_selector("a", :href => mark_as_normal_item_path(@item))
+    end
+    
+    it "should not render 'mark as repaired' link if not damaged" do
+      as_item_owner
+      @item.stub(:damaged?).and_return(false)
+      render
+      rendered.should_not have_selector("a", :href => mark_as_normal_item_path(@item))
     end
   end
 end
