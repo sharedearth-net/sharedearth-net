1195d0a51ab3f2ad919b188c536f73072a8ec0e9
Nebojsa <zoricn@gmail.com>
2011-06-16 13:42:10 +0200

    Added search controller for multiple models

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,33 @@
+class SearchController < ApplicationController
+  def index
+  
+    unless params[:search].empty?
+      case params[:type]
+        when 'items'
+          @items  = Item.search(params[:search], current_user.person.id)
+          @people = []
+        when 'people'
+          @people = Person.search(params[:search])
+          @items  = []
+        when 'all'
+          @items = Item.search(params[:search], current_user.person.id)
+          @people = Person.search(params[:search])
+        else
+          @items = Item.search(params[:search], current_user.person.id)
+          @people = []
+          @people = Person.search(params[:search]) if @items.empty?
+          if !@items.empty?
+            params[:type] = 'items'
+          elsif !@people.empty?
+            params[:type] = 'people'
+          else
+            params[:type] = 'all'
+          end
+      end 
+    else
+      @items  = []
+      @people = []
+    end
+  end
+
+end
