6cf10627c836494427b1c2a2666b873aa18e227f
Nebojsa <zoricn@gmail.com>
2011-06-16 13:46:16 +0200

    updated cucumber step

--- features/step_definitions/extended_steps.rb
+++ features/step_definitions/extended_steps.rb
@@ -1,7 +1,7 @@
 #I should see "Bike" 3 times
-Then /^(?:|I )should see \/([^\/]*)\/ (\d+)(?:x|X| times?)?$/ do |regexp, count|
+Then /^I should see "([^"]*)" (\d+) times$/ do |regexp, count|
   regexp = Regexp.new(regexp)
   count = count.to_i
-  page.find(:xpath, '//body').text.split(regexp).length.should == count+1
+  page.find(:xpath, '//body').text.split(regexp).length.should == count
 end
 
