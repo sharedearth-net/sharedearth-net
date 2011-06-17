90723a55cd13e012a5528bff89efe60d2d06364a
Nebojsa <zoricn@gmail.com>
2011-06-17 20:42:37 +0200

    is in the network sentence moved to helper

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -14,4 +14,19 @@ module PeopleHelper
  def extended_friends
    params[:type] == 'extended'
  end
+ 
+ def in_network?(current_person, other_person )
+   	if current_person.trusts?(other_person)
+			image = image_tag("ic-trusted-net-you-green.png", :class => "icons", :alt => "Trusted Profile")
+			link = link_to 'In your trusted network', (network_person_path(other_person))
+	  else
+			image = image_tag("ic-trusted-net-you-orange.png", :class => "icons", :alt => "Untrusted Profile") 
+					if other_person.requested_trusted_relationship?(current_person)
+						 link = "Establish trust request pending"
+					else
+					   link = link_to 'Not in your trusted network', (network_person_path(other_person)) 
+					end
+		end
+		"#{image}<p>#{link}</p>".html_safe
+ end
 end
