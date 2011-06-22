35707bd1bcc977389997f876c873feb7a82ea5f8
Nebojsa <zoricn@gmail.com>
2011-06-22 12:29:23 +0200

    req. model inv.

---
+++ app/models/requested_invitation.rb
@@ -0,0 +1,12 @@
+class RequestedInvitation < ActiveRecord::Base
+  has_one :user
+  #validate :user_is_unique
+
+  def user_is_unique
+    # Get the current count of objects having this link
+    count = RequestedInvitation.count(:conditions => ['user_id = ?', self.user_id])
+
+    # Add an error to the model if the count is not zero
+    errors.add_to_base("You already requested invitation") unless count == 0
+  end
+end
