3f18257fec56d567f8fef0299aa2d4d5f7b83668
Nebojsa <zoricn@gmail.com>
2011-06-27 11:26:31 +0200

    Redirect based on comment model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,15 @@ class CommentsController < ApplicationController
   model_name = params[:comment][:commentable_type]
   record_commentable = model_name.constantize.find(params[:comment][:commentable_id])
   record_commentable.comments.create(:commentable => record_commentable, :user_id => current_user.id, :comment => params[:comment][:comment] )
-  redirect_to request_path(record_commentable)
+  case model_name
+    when "ItemRequest"
+      redirect_to request_path(record_commentable)
+    when "Item"
+      redirect_to item_path(record_commentable)
+    when "EventLog"
+      redirect_to dashboard_path
+    else
+     #  
+  end
  end
 end
