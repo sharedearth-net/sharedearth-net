8b6e014318dde3169d98d08937a2626e58c2b557
Nebojsa <zoricn@gmail.com>
2011-07-04 18:05:36 +0200

    Added responders to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,29 @@ class CommentsController < ApplicationController
  def create
   model_name = params[:comment][:commentable_type]
   record_commentable = model_name.constantize.find(params[:comment][:commentable_id])
-  record_commentable.comments.create(:commentable => record_commentable, :user_id => current_user.id, :comment => params[:comment][:comment] )
+  @comment = record_commentable.comments.create(:commentable => record_commentable, :user_id => current_user.id, :comment => params[:comment][:comment] )
+  
+  respond_to do |format|
+      format.html { redirection_rules(model_name) }
+      format.json  { render :json => @comment }
+  end
+=begin
   case model_name
+    when "ItemRequest"
+      redirect_to request_path(record_commentable)
+    when "Item"
+      redirect_to item_path(record_commentable)
+    when "EventLog"
+      redirect_to dashboard_path
+    else
+     #  
+  end
+=end
+ end
+ 
+ private
+ def redirection_rules(model_name)
+   case model_name
     when "ItemRequest"
       redirect_to request_path(record_commentable)
     when "Item"
