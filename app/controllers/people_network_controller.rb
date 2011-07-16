7acc631fdb2307eaa7d42540daad20bf0da816a9
Nebojsa <zoricn@gmail.com>
2011-07-16 12:58:30 +0200

    create event log so the dashboard will know who witdrawn the trust

--- app/controllers/people_network_controller.rb
+++ app/controllers/people_network_controller.rb
@@ -5,7 +5,11 @@ class PeopleNetworkController < ApplicationController
   def destroy
     @people_network = PeopleNetwork.find(params[:id])
     trusted_person = @people_network.trusted_person
-    EventLog.create_news_event_log(@people_network.person, @people_network.trusted_person, nil, EventType.trust_withdrawn, @people_network)
+    if @people_network.person == current_user.person
+      EventLog.create_news_event_log(@people_network.person, @people_network.trusted_person, nil, EventType.trust_withdrawn, @people_network)
+    else
+      EventLog.create_news_event_log(@people_network.trusted_person, @people_network.person, nil, EventType.trust_withdrawn, @people_network)
+    end
     PeopleNetwork.involves(@people_network.person).involves(@people_network.trusted_person).limit(2).destroy_all
     @people_network.person.reputation_rating.decrease_trusted_network_count
     @people_network.trusted_person.reputation_rating.decrease_trusted_network_count
