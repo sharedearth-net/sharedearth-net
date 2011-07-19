5e0f5be9a99a244c304be75fc32641d6d77e3ac0
Nebojsa <zoricn@gmail.com>
2011-07-19 17:08:44 +0200

    user and item model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,7 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.nickname = auth["user_info"]["nickname"]
-      user.create_person(:name => auth["user_info"]["name"])
+      user.create_person(:name => auth["user_info"]["name"], :email => auth["extra"]["email"])
       user.person.create_reputation_rating(:gift_actions => 0,:distinct_people => 0, 
                               :total_actions => 0, :positive_feedback => 0, :negative_feedback => 0, :neutral_feedback => 0,
                               :requests_received => 0,  :requests_answered => 0, :trusted_network_count => 0,  :activity_level => 0)
