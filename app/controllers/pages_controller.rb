0b6b3e90b390d73e5dba9f9f421934ce316383da
Juan Hawa <juan.hawa.dev@gmail.com>
2011-07-25 11:04:29 -0400

    Specified with layout to use in the about template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,6 +17,7 @@ class PagesController < ApplicationController
                                                                              :order => 'created_at DESC', :include => [:event_log])
    end
 
-   def about
-   end
+  def about
+    render :layout => 'shared_earth'
+  end
 end
