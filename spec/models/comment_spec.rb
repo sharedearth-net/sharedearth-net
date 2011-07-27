67c7701ee1049ad697ff81469ddcbc5168b4328c
Juan Hawa <juan.hawa.dev@gmail.com>
2011-07-27 17:41:43 -0400

    Bug fixing: the discussion elements were being fetched incorrectly, this was because the 'comments' property on the ItemRequest model is not an association, but an array

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -16,16 +16,4 @@ describe Comment do
       end
     end
   end
-
-  describe "#ordered_by_creation_desc" do
-    before :each do
-      @first_comment  = Factory.create(:comment, :created_at => Time.now)
-      @second_comment = Factory.create(:comment, :created_at => (Time.now + 2.days))
-    end
-    
-    it "should return a list of comments ordered by creation date in desc order" do
-      comments = Comment.ordered_by_creation_desc
-      comments.should == [@second_comment, @first_comment]
-    end
-  end
 end
