5f4cca3a77d0aacc5d22c0fdcd630d16a32d1f93
Juan Hawa <juan.hawa.dev@gmail.com>
2011-08-05 01:34:58 -0400

    Made S3 bucket name be queryed from a Config var

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -2,7 +2,10 @@ require 'paperclip_wrapper'
 
 # http://docs.heroku.com/config-vars
 if Rails.env.production?
-  S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => "sharedearth-production"}
+  S3_CREDENTIALS = { 
+    :access_key_id      => ENV['S3_KEY'], 
+    :secret_access_key  => ENV['S3_SECRET'], 
+    :bucket             => ENV['S3_BUCKET'] }
 else
   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
 end
