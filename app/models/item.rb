b097daf460d60998befefaaa0ee67408ec96c5fe
Juan Hawa <juan.hawa.dev@gmail.com>
2011-08-08 18:38:54 -0400

    [Bug #16521429] Don't show deleted items on search results
    Don't show deleted items on search results
    
    Don't show 'request this item' link if the item has been deleted

--- app/models/item.rb
+++ app/models/item.rb
@@ -88,7 +88,7 @@ class Item < ActiveRecord::Base
         item_name_like = words.map{|k| "upper(name) LIKE upper('%#{k}%')" }.join(" AND ")
         item_desc_like = words.map{|k| "upper(description) LIKE upper('%#{k}%')" }.join(" AND ")
         sql_like = "(" + item_type_like + ") OR (" + item_name_like + ") OR (" + item_desc_like + ")"
-        search_sql = "select id from items where id in (#{items_sql}) and (#{sql_like})"
+        search_sql = "select id from items where id in (#{items_sql}) and (#{sql_like}) and deleted_at IS NULL"
         items_ids = ActiveRecord::Base.connection.execute(search_sql) 
  
         ids = items_ids.map{ |i| i["id"] }
