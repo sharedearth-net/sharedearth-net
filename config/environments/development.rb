191159dc122d30c70a78e48352ed37af17187899
Nebojsa <zoricn@gmail.com>
2011-08-25 19:49:01 +0200

    Setting up everything for email to be sent

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,7 @@ Sharedearthapp::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  config.action_mailer.delivery_method = :ses
 end
 
---
+++ config/initializers/amazon_ses.rb
@@ -0,0 +1,3 @@
+ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
+  :access_key_id     => 'AKIAJDPPPEEFNVBFDP7A',
+  :secret_access_key => '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'
