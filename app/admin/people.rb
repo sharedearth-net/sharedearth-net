d794826b5304c0ebe98490f08bccaf973ded8d0b
Nebojsa Zoric <zoricn@gmail.com>
2011-09-09 18:01:04 +0200

    Manage people in the application

---
+++ app/admin/people.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Person do
+  index do
+     column "Name", :name, :sortable => false
+     column "Authorised", :authorised_account
+     column "E-mail", :email
+     column "Invitations count", :invitations_count, :sortable => false
+     default_actions
+  end
+
+		form do |f|
+      f.inputs "Details" do
+        f.input :name
+        f.input :email, :label => "E-mail"
+        f.input :invitations_count
+      end
+      f.buttons
+    end
+  
+end
