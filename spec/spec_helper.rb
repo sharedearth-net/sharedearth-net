2f722602cf10bf335ff69d6f97660ae1fdf11cc7
Nebojsa Zoric <zoricn@gmail.com>
2011-09-16 21:15:23 +0200

    Spec's helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -80,3 +80,17 @@ module UserSpecHelper
     }
   end
 end
+
+module Spec
+  module Mocks
+    module Methods
+      def stub_association!(association_name, methods_to_be_stubbed = {})
+        mock_association = Spec::Mocks::Mock.new(association_name.to_s)
+        methods_to_be_stubbed.each do |method, return_value|
+          mock_association.stub!(method).and_return(return_value)
+        end
+        self.stub!(association_name).and_return(mock_association)
+      end
+    end
+  end
+end
