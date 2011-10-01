4e5732df437d2c9c9f107a6fbd16a0181478d9c1
Nebojsa <zoricn@gmail.com>
2011-10-01 16:51:55 +0200

    Sen amazon ses keys moved to environment variables

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  :access_key_id     => 'AKIAJDPPPEEFNVBFDP7A',
-  :secret_access_key => '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'
+  :access_key_id     => ENV['SEN_SES_KEY'],
+  :secret_access_key => ENV['SEN_SES_SECRET']
