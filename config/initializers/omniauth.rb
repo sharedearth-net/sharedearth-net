47425cc2c08ae4584e2a2fd196e4a78ccad64f71
Nebojsa <zoricn@gmail.com>
2011-10-01 16:51:27 +0200

    Facebook key and secret moved to environment variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '216097298441765', 'cb27474f079a8ae3a07e592c7d8fb2e1', {:scope => 'publish_stream,email', :client_options => {:ssl => {:ca_path => '/etc/ssl/certs'}}}
+  provider :facebook, ENV['SEN_FACEBOOK_APP_ID'], ENV['SEN_FACEBOOK_APP_SECRET'], {:scope => 'publish_stream,email', :client_options => {:ssl => {:ca_path => '/etc/ssl/certs'}}}
 end
 if Rails.env.development?
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE 
