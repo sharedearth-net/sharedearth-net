b7ab570fbff276038533898e890e777be06602b1
Nebojsa <zoricn@gmail.com>
2011-10-05 13:31:42 +0200

    Added root_url to be more generic

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
                                            :trusted_network_count => 0,  :activity_level => 0)
     end
     msg  = "has connected to sharedearth.net."
-    link = "http://sharedearth.net"
+    link = root_url
 
     FbService.post_on_my_wall(auth["credentials"]["token"], msg, link, :append_name => true)
     User.find_by_uid(auth["uid"])
