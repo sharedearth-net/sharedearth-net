6ccf8fd7b37ecaae55e3dba1cc970db42d7bdfec
Nebojsa Zoric <zoricn@gmail.com>
2011-10-21 13:07:54 +0200

    Search steps updated, and profile review

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -3,7 +3,8 @@ Given /^(.+) has not accepted the Privacy Policy$/ do |model_name|
   person.update_attributes(:accepted_pp => false)
 end
 
-Given /^(.+) is not authorized$/ do |model_name|
-  person = model(model_name)
-  person.update_attributes(:authorised_account => false)
+Given /^"(.+)" is not authorized$/ do |model_name|
+  @person = Person.find_by_name(model_name)
+  @person.authorised_account = false
+  @person.save!
 end
