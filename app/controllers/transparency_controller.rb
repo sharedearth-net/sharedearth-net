210ba321521a88b4ec276fbb12e69b2c13d156f6
Nebojsa Zoric <zoricn@gmail.com>
2011-10-25 17:44:28 +0200

    Transparent controller protection added

--- app/controllers/transparency_controller.rb
+++ app/controllers/transparency_controller.rb
@@ -1,19 +1,19 @@
 class TransparencyController < ApplicationController
   layout :dynamic_layout
   
-  def dynamic_layout
-    if current_user.nil? || !current_user.person.authorised_account
-      'shared_earth'
-    else
-      'application'
-    end
-  end
-
   def index
   end
   
   def accept_tr
     current_user.person.accept_tr!
     redirect_to next_policy_path
+  end
+  
+  def dynamic_layout
+    if current_user.nil?  || !current_user.person.authorised_account
+      'shared_earth'
+    else
+      'application'
+    end
   end
 end
