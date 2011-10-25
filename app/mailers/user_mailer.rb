692efc79734c4ceed31983a936413b088cb59b8f
Nebojsa Zoric <zoricn@gmail.com>
2011-10-25 17:43:50 +0200

    Removed sensitive data

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,11 +13,4 @@ class UserMailer < ActionMailer::Base
     mail(:to => "#{email}", :subject => "Your sharedearth.net invitation")
   end
 
-  def testing
-    ses = AWS::SES::Base.new( :access_key_id     => 'AKIAJDPPPEEFNVBFDP7A', :secret_access_key => 'PVRJ8TtOwwhPjdxyOy5v/OYMxLHmz5QPRKdnLBlc' )
-    ses.send_email :to        => ['zoricn@gmail.com'],
-                   :source    => '"Steve Smith" <noreply@sharedearth.net>',
-                   :subject   => 'Testing Amazon SES',
-                   :text_body => 'Totaly original body of email'
-  end
 end
