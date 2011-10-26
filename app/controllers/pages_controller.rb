e151283de285081fe19e43a3dfbeea4df85aa7ec
Nebojsa Zoric <zoricn@gmail.com>
2011-10-26 12:47:37 +0200

    About and contribute page fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,12 +26,12 @@ class PagesController < ApplicationController
   end
 
   def about
-    about_layout = ((current_user.nil?  || !current_user.person.authorised_account) ? 'shared_earth' : 'application')
+    about_layout = ((current_user.nil?  || !current_user.person.authorised_account || !current_user.person.accepted_pp?) ? 'shared_earth' : 'application')
     render :layout => about_layout
   end
 
   def contribute
-    contribute_layout = ((current_user.nil?  || !current_user.person.authorised_account) ? 'shared_earth' : 'application')
+    contribute_layout = ((current_user.nil?  || !current_user.person.authorised_account || !current_user.person.accepted_pp?) ? 'shared_earth' : 'application')
     render :layout => contribute_layout
   end
 
