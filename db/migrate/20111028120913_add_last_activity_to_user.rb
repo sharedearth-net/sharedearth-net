80e10417da836b7139c6f1a897657adf42bc9504
Nebojsa Zoric <zoricn@gmail.com>
2011-11-01 15:31:26 +0100

    When adding new field update last activity for all user to time of deployment

--- db/migrate/20111028120913_add_last_activity_to_user.rb
+++ db/migrate/20111028120913_add_last_activity_to_user.rb
@@ -1,6 +1,10 @@
 class AddLastActivityToUser < ActiveRecord::Migration
   def self.up
     add_column :users, :last_activity, :datetime
+    users = User.all
+    users.each do |u|
+      u.update_attributes(:last_activity => Time.now)
+    end
   end
 
   def self.down
