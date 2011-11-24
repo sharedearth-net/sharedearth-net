4fdafd0781d5a5e8d204f899fa8d83bf155a678f
Nebojsa Zoric <zoricn@gmail.com>
2011-11-24 12:20:45 +0100

    people controller spec

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe PeopleController do
-  
+
   let(:signedin_user) { generate_mock_user_with_person }
   let(:mock_person)   { mock_model(Person).as_null_object }
   let(:mock_items)    { [ mock_model(Item, :name => "Item1").as_null_object, mock_model(Item, :name => "Item2").as_null_object ] }
   let(:mock_item_requests)    { [ mock_model(ItemRequest).as_null_object, mock_model(ItemRequest).as_null_object ] }
-  
+
   it_should_require_signed_in_user_for_actions :show, :edit, :update, :cancel
-  
+
   describe "for signed in member" do
     before do
       sign_in_as_user(signedin_user)
@@ -47,15 +47,15 @@ describe PeopleController do
         Person.stub(:find_by_id).with("37") { mock_person }
         get :show, :id => "37"
       end
-      
+
       it "assigns the requested person as @person" do
-        assigns(:person).should be(mock_person)
+        assigns(:person).should be(rock_person)
       end
-      
+
       it "should render show template" do
         response.should render_template("show")
       end
-      
+
       it "should assign person's items as @items" do
         assigns(:items).should == mock_person.items.without_deleted.sort_by{|i| i.item_type.downcase}
       end
@@ -65,7 +65,7 @@ describe PeopleController do
       end
 
     end
-    
+
     describe "GET edit" do
 
       before do
@@ -73,12 +73,12 @@ describe PeopleController do
         mock_person.stub(:belongs_to?).and_return(true)
         Person.stub(:find_by_id).with("37") { mock_person }
       end
-      
+
       it "assigns the requested person as @person" do
         get :edit, :id => "37"
         assigns(:person).should be(mock_person)
       end
-      
+
       it "should render show template" do
         get :edit, :id => "37"
         response.should render_template("edit")
@@ -107,14 +107,14 @@ describe PeopleController do
         mock_person.stub(:belongs_to?).and_return(true)
         Person.stub(:find_by_id).with("37") { mock_person }
       end
-      
+
       describe "with valid params" do
 
         it "updates the requested person" do
           mock_person.should_receive(:update_attributes).with({'these' => 'params'})
           put :update, :id => "37", :person => {'these' => 'params'}
         end
-        
+
         it "assigns the requested person as @person" do
           mock_person.stub(:update_attributes).and_return(true)
           put :update, :id => "37"
@@ -131,7 +131,7 @@ describe PeopleController do
         it "should allow access only if person being updated is currently signed in" do
           mock_person.should_receive(:belongs_to?).with(signedin_user).and_return(true)
           put :update, :id => "37"
-        
+
           assigns(:person).should be(mock_person)
         end
 
@@ -142,7 +142,7 @@ describe PeopleController do
           flash[:alert].should eql(I18n.t('messages.you_cannot_edit_others'))
           response.should redirect_to(root_path)
         end
-     
+
         context "When the user has reviewed its profile" do
           before :each do
             mock_person.stub!(:has_reviewed_profile?).and_return(true)
@@ -172,15 +172,15 @@ describe PeopleController do
           mock_person.has_reviewed_profile.should be_true
         end
       end
-      
+
       describe "with invalid params" do
-      
+
         it "assigns the person as @person" do
           mock_person.stub(:update_attributes).and_return(false)
           put :update, :id => "37"
           assigns(:person).should be(mock_person)
         end
-      
+
         it "re-renders the 'edit' template" do
           mock_person.stub(:update_attributes).and_return(false)
           put :update, :id => "37"
