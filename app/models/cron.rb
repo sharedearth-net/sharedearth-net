571363838bee87a4f24c08f613ef3fd7e8b297a1
Nebojsa Zoric <zoricn@gmail.com>
2011-12-06 11:52:54 +0100

    For testind purpoces disabled logging of number of emails sent

--- app/models/cron.rb
+++ app/models/cron.rb
@@ -10,7 +10,7 @@ class Cron
 		      UserMailer.notify_with_recent_activity(@recent_activity_logs, p.email, p.user).deliver
 		      email_notification = EmailNotification.create!(:person_id => p.id, :email => p.email)
 		      @recent_activity_logs.each { |a| a.log_notification_email!(email_notification) }
-          p.increase_email_notification_count!
+          #p.increase_email_notification_count!
           p.log_email_notification_time!
 		    rescue Exception => e
 		      puts "Email sending failed to " + p.email
