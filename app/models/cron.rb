6c200cb87d8d6ec5bc039ead133fd6dbfa684f23
Nebojsa Zoric <zoricn@gmail.com>
2011-12-07 20:40:45 +0100

    removed confitions so email is always sent - for testing purpoces

--- app/models/cron.rb
+++ app/models/cron.rb
@@ -4,14 +4,14 @@ class Cron
     unactive_user_ids = User.unactive.collect(&:id)
     @persons = Person.notification_candidate.with_smart_notifications#.include_users(unactive_user_ids)
     @persons.each do |p| 
-      @recent_activity_logs = ActivityLog.include_person(p).unread.email_not_sent
+      @recent_activity_logs = ActivityLog.include_person(p).unread#.email_not_sent
       unless @recent_activity_logs.nil? || @recent_activity_logs.empty?
 				begin
 		      UserMailer.notify_with_recent_activity(@recent_activity_logs, p.email, p.user).deliver
 		      email_notification = EmailNotification.create!(:person_id => p.id, :email => p.email)
-		      @recent_activity_logs.each { |a| a.log_notification_email!(email_notification) }
+		      #@recent_activity_logs.each { |a| a.log_notification_email!(email_notification) }
           #p.increase_email_notification_count!
-          p.log_email_notification_time!
+          #p.log_email_notification_time!
 		    rescue Exception => e
 		      puts "Email sending failed to " + p.email
 		    end
