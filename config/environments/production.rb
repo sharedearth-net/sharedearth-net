09f1892b5c76d76b0e9b2a5839c5f99e42f2f0db
Nebojsa Zoric <zoricn@gmail.com>
2011-12-08 10:03:59 +0100

    Setup main host for email server to use heroku vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,4 +50,6 @@ Sharedearthapp::Application.configure do
   Sass::Plugin.options[:never_update] = true
   
   config.action_mailer.delivery_method = :ses
+  config.action_mailer.default_url_options = { :host => ENV['domain_url'] }
+
 end
