f3323431410aa4f2aabb0ffee1eaeffcb230eaae
Nebojsa Zoric <zoricn@gmail.com>
2011-12-09 13:08:51 +0100

    remove recent activity if perforemed by user extracted

--- app/models/cron.rb
+++ app/models/cron.rb
@@ -5,9 +5,9 @@ class Cron
     @persons = Person.notification_candidate.with_smart_notifications.include_users(unactive_user_ids)
     @persons.each do |p| 
       @recent_activity_logs = ActivityLog.include_person(p).unread.email_not_sent
+      @recent_activity_logs.delete_if {|a| a.recent_activity_subject?(p)}
       unless @recent_activity_logs.nil? || @recent_activity_logs.empty?
 				begin
-          @recent_activity_logs.delete_if {|a| a.recent_activity_subject?(p)}
 		      UserMailer.notify_with_recent_activity(@recent_activity_logs, p.email, p.user).deliver
 		      email_notification = EmailNotification.create!(:person_id => p.id, :email => p.email)
 		      @recent_activity_logs.each { |a| a.log_notification_email!(email_notification) }
