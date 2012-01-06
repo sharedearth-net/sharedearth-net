0640100f74a6b8c28451ee4696e4d6eba3638874
Amit Ambardekar <amitamb@gmail.com>
2012-01-06 21:48:32 +0530

    Added network status info to the bottom of recent activity notification email

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,20 @@ module ApplicationHelper
       people_network_request.person.avatar(options[:size])
     end
   end
+
+  def network_status_trusted_network(person = nil, only_path = true, link_options = {})
+    person ||= current_person
+    link_to pluralize(person.trusted_network_size, 'person') + " in your trusted network", network_person_path(person, :only_path => only_path), link_options
+  end
+
+  def network_status_personal_network(person = nil, only_path = true, link_options = {})
+    person ||= current_person
+    link_to pluralize(person.personal_network_size, 'person') + " in your entire network", my_network_person_path(person, :only_path => only_path), link_options
+  end
+
+  def network_status_items(person = nil)
+    person ||= current_person
+    pluralize(person.personal_network_items_count, 'item') + " available in your network"
+  end
+
 end
