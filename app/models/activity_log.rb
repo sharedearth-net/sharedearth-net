7b294d884bcb1ad8e7d35733973c2c145d2a7ece
Nebojsa Zoric <zoricn@gmail.com>
2012-02-01 10:43:59 +0100

    New shareage activity log request

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -24,18 +24,18 @@ class ActivityLog < ActiveRecord::Base
   # [primary] accepted [secondary]'s [related] your [action_object]
   # Note: this just illustrates what primary, secondary, related and action_object are.
   #       Technically speaking sentence is not generated like that.
-  
+
   #Action object and related object is not obligatory when two persons are establishing trust
   validates_presence_of :primary_id, :primary_type
   validates_presence_of :event_code
   #validates_presence_of :action_object_id, :action_object_type, :action_object_type_readable
   #validates_presence_of :related_id, :related_type
-  
+
   scope :gift_actions, lambda { |entity| where("(primary_id = ? and event_type_id IN (?) ) ", entity.id, EventType.completed_request_ids) }
   scope :involves, lambda { |entity| where("(primary_id = ? AND primary_type = ?) OR (secondary_id = ? AND secondary_type = ?) ", entity.id, entity.class.to_s, entity.id, entity.class.to_s) }
   scope :completed_requests, lambda { |entity| where("event_type_id IN (?)", EventType.completed_request_ids) }
   scope :personal_actions, lambda { |entity| where("event_type_id IN (?)", EventType.personal_actions) }
-  scope :activities_involving, lambda { |p1,p2| where("(primary_id = ? AND primary_type = ? AND secondary_id = ? AND secondary_type = ? and event_type_id IN (?)) ", 
+  scope :activities_involving, lambda { |p1,p2| where("(primary_id = ? AND primary_type = ? AND secondary_id = ? AND secondary_type = ? and event_type_id IN (?)) ",
                                                       p1.id, p1.class.to_s, p2.id, p2.class.to_s, EventType.current_actions_underway) }
   scope :public_activities, lambda { |entity| where("(primary_id = ? AND primary_type = ? and event_type_id IN (?) ) ", entity.id, entity.class.to_s, EventType.current_actions_underway) }
   scope :item_public_activities, lambda { |entity| where("(action_object_id = ? AND action_object_type = ? and event_type_id IN (?) ) ", entity.id, entity.class.to_s, EventType.current_actions_underway_items) }
@@ -83,13 +83,43 @@ class ActivityLog < ActiveRecord::Base
       :event_type_id => EventType.new_item_request_requester
     )
   end
-  
+
+  def self.create_new_shareage_item_request_activity_log(item_request)
+    event_code = ActivityLog.next_event_code
+
+    # create log for gifter
+    ActivityLog.create!(
+      :event_code => event_code,
+      :primary => item_request.gifter,
+      :action_object => item_request.item,
+      :action_object_type_readable => item_request.item.item_type,
+      :secondary => item_request.requester,
+      :secondary_short_name => item_request.requester.first_name,
+      :secondary_full_name => item_request.requester.name,
+      :related => item_request,
+      :event_type_id => EventType.new_shareage_item_request_gifter
+    )
+
+    # create log for requester
+    ActivityLog.create!(
+      :event_code => event_code,
+      :primary => item_request.requester,
+      :action_object => item_request.item,
+      :action_object_type_readable => item_request.item.item_type,
+      :secondary => item_request.gifter,
+      :secondary_short_name => item_request.gifter.first_name,
+      :secondary_full_name => item_request.gifter.name,
+      :related => item_request,
+      :event_type_id => EventType.new_shareage_item_request_requester
+    )
+  end
+
   # One method to log all activity
   # First param: item_request object from which activity log will be created
   # Second param: Type of the event for gifter
-  # Third param: Type of the event for the requester 
+  # Third param: Type of the event for the requester
   # #####
-  
+
   def self.create_item_request_activity_log(item_request, event_type_gifter, event_type_requester)
     event_code = ActivityLog.next_event_code
 
@@ -133,7 +163,7 @@ class ActivityLog < ActiveRecord::Base
        :event_type_id => EventType.add_item
      )
    end
-   
+
    def self.create_person_join_activity_log(person)
      event_code = ActivityLog.next_event_code
 
@@ -147,11 +177,11 @@ class ActivityLog < ActiveRecord::Base
        :event_type_id => EventType.new_person_join
      )
    end
-   
+
   # More general method for saving activity logs
   # First param: item_request object from which activity log will be created
   # Second param: Type of the event for gifter
-  # Third param: Type of the event for the requester 
+  # Third param: Type of the event for the requester
   # #####
   #THIS IS ONLY USED WHEN TWO PERSONS CONFIRMS RELATIONSHIP, SECOND PERSON IS THE ONE WHO CONFIRMS TRUST
   def self.create_activity_log(first_person, second_person, object, event_type_requester, event_type_gifter)
@@ -161,15 +191,15 @@ class ActivityLog < ActiveRecord::Base
       second_person_name = nil
     else
       second_person_first_name = second_person.first_name
-      second_person_name = second_person.name    
+      second_person_name = second_person.name
     end
-    
+
     if(first_person.nil?)
       first_person_first_name = nil
       first_person_name = nil
     else
       first_person_first_name = first_person.first_name
-      first_person_name = first_person.name    
+      first_person_name = first_person.name
     end
     if(object.nil?)
       action_object_type_readable = nil
@@ -285,8 +315,8 @@ class ActivityLog < ActiveRecord::Base
       person
     when 17
       true
-       
-      
+
+
     when 27
       true
     when 28
