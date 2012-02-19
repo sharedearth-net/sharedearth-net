e52dfe4fe8cfff670c96f213fb03c9cd32dd2e49
Amit Ambardekar <amitamb@gmail.com>
2012-02-19 23:04:53 +0530

    Removed entity_types table and related model as well as test code

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,6 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-entity_types = [
-								{:id => 1, :name => 'Person'},
-								{:id => 2, :name => 'Item'},
-								{:id => 3, :name => 'Skill'},
-								{:id => 4, :name => 'Village'},
-								{:id => 5, :name => 'Community'},
-								{:id => 6, :name => 'Project'},
-								{:id => 7, :name => 'Trusted Person'},
-								{:id => 8, :name => 'Mutual Person'}
-							 ]
-
-EntityType.delete_all
-
-entity_types.each do |entity|
- et = EntityType.new(:entity_type_name => entity[:name])
- et.id = entity[:id]
- et.save!
-end
-
 AdminUser.create!(:email    => 'admin@senlocal.heroku.net', 
                   :password => 'password', 
                   :password_confirmation => 'password')
