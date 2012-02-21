14afbf37db523eb50b7d483d5793c70eedc60021
Nebojsa Zoric <zoricn@gmail.com>
2012-02-21 12:39:41 +0100

    Add shareage event log

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120130153359) do
+ActiveRecord::Schema.define(:version => 20120221103208) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
