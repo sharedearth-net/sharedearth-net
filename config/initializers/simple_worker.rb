bc10132654bf9f6b36c4090a0523ab7079200edc
Nebojsa Zoric <zoricn@gmail.com>
2012-03-30 16:46:06 +0200

    Removed simple worker

--- config/initializers/simple_worker.rb
+++
@@ -1,9 +0,0 @@
-SimpleWorker.configure do |config|
-    config.token = ENV['SIMPLE_WORKER_TOKEN']
-    config.project_id = ENV['SIMPLE_WORKER_PROJECT_ID']
-    # Use the line below if you're using an ActiveRecord database
-    config.database = Rails.configuration.database_configuration[Rails.env]
-	  config.auto_merge = true
-  	config.unmerge_gem('delayed_job')
-  	config.unmerge_gem('devise')
-end
