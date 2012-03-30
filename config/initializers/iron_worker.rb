de3b0e3a86d81542b17fff7d12e2b2a6d65431ae
Nebojsa Zoric <zoricn@gmail.com>
2012-03-30 16:45:32 +0200

    Iron worker initializer

---
+++ config/initializers/iron_worker.rb
@@ -0,0 +1,7 @@
+IronWorker.configure do |config|
+    config.token = ENV['SIMPLE_WORKER_TOKEN']
+    config.project_id = ENV['SIMPLE_WORKER_PROJECT_ID']
+    #config.auto_merge = true
+    # Use the line below if you're using an ActiveRecord database
+    config.database = Rails.configuration.database_configuration[Rails.env]
+end
