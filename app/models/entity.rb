2d98f4408f5c6375ceb0a20daad70041142c375f
Nebojsa Zoric <zoricn@gmail.com>
2012-04-07 18:38:53 +0200

    entity scopes

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,4 +3,14 @@ class Entity < ActiveRecord::Base
   belongs_to :specific_entity, :polymorphic => true
 
   scope :item, lambda { |entity| where("specific_entity_type =? AND specific_entity_id = ?", "Item", entity.id) }
+  scope :villages_with_ids, lambda { |village_ids| where("specific_entity_id IN (?) AND specific_entity_type = ?", village_ids, "Village")}
+
+  GROUP_ENTITIES = ["Village"]
+
+  #Find entities that are connected with person involved, update with more generic, pairing up id and entity type
+  def self.groups_with_person(person)
+    entities = []
+    village_ids = HumanNetwork.entity_types(GROUP_ENTITIES).member(person).map(&:entity_id)
+    entities += self.villages_with_ids(village_ids)
+  end
 end
