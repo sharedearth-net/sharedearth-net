3155b7e2daad54210459da0334ab279ed5473886
Akram Khan <akramkhan83@gmail.com>
2012-11-12 17:30:27 +0530

    improved on the trust profile page

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -21,17 +21,15 @@ module PeopleHelper
  
  def in_network?(current_person, other_person )
    	if current_person.trusts?(other_person)
-			image = image_tag("ic-trusted-net-you-green.png", :class => "icons", :alt => "Trusted Profile")
-			link = link_to 'In your trusted network', (network_person_path(other_person))
-	  else
-			image = image_tag("ic-trusted-net-you-orange.png", :class => "icons", :alt => "Untrusted Profile") 
-					if other_person.requested_trusted_relationship?(current_person)
-						 link = "Trust request pending"
-					else
-					   link = link_to 'Not in your trusted network', (network_person_path(other_person)) 
-					end
+		link = link_to 'In your trusted network', (network_person_path(other_person))
+	else	
+		if other_person.requested_trusted_relationship?(current_person)
+			 link = "Trust request pending"
+		else
+		   link = link_to 'Not in your trusted network', (network_person_path(other_person)) 
 		end
-		"#{image}<p>#{link}</p>".html_safe
+	end
+		# "#{image}<p>#{link}</p>".html_safe
  end
  
  def trusted_person_class(person)
