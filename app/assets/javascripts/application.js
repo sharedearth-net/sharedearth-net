e36b325cb821b14c06a08ccbe1dc16a674931144
Akram Khan <akramkhan83@gmail.com>
2012-11-15 19:32:44 +0530

    settings icon of the header is functional

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,30 @@ $(document).ready(function() {
 });
 
 
+$(document).ready(function(){
+	$("#settings-dd").click(function(event) {
+		return false;
+	});
+
+	$('#settings-menu').click(function(event) {
+		event.preventDefault();
+		$('#settings-dd').toggle();
+		return false;
+	});
+	 $(function() {
+        $( "#tabs" ).tabs();
+    });
+	  $(function() {
+        $( "#tabs" ).tabs();
+    });
+   	$('input.feedback').screwDefaultButtons({
+        image: 'url("img/icons/feedback.png")',
+        width: 123,
+        height: 41
+    });
+});
+
+
 
 /* textbox class on request-page */
 
