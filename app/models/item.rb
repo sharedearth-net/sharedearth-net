ff6945af10371951c62e4f3d30443a24667ea488
haseeb <engg.haseeb@gmail.com>
2013-04-09 05:37:33 -0400

    heroku exception fixed

--- app/models/item.rb
+++ app/models/item.rb
@@ -417,7 +417,7 @@ class Item < ActiveRecord::Base
   end
 
   def self.item_list_sorted
-    Item.all(:group => "item_type", :order => "item_type ASC", :order => "count(*) DESC ")
+    Item.select(:group => "item_type", :order => "item_type ASC", :order => "count(*) DESC ")
     #Item.group('item_type').order('count(*) DESC').order('item_type ASC')
   end
 
